7670b49608142800d1e1fbc554dcb8413c724484
Jakub Czajkowski <kubaczajkowski25@gmail.com>
2022-07-16 04:15:58 +0200

    init

--- src/environments/apiurl.ts
+++ src/environments/apiurl.ts
@@ -1,3 +1,4 @@
-const apiUrl = "http://localhost:8080/api";
+// const apiUrl = "http://localhost:8080/api"; //develop
+const apiUrl = "https://finance-monitor-api.herokuapp.com/api"; //prod
 
 export default apiUrl
